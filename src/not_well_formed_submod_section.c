d6d01135a39baaa26d408567cb731848a8c77ca6
Laurence Lundblade <lgl@securitytheory.com>
2021-03-27 03:42:43 -0700

    submod improvements

--- src/not_well_formed_submod_section.c
+++ src/not_well_formed_submod_section.c
@@ -1,9 +1,13 @@
 
 /*
-The submod section is an integer lacking it argument instead of a map
+The submod section is an integer lacking its CBOR argument. That is the
+integer is truncated. The CBOR decoder should find that the submodules
+section is not well formed. 
+
+(In theory some CBOR decoders might  
 */
 
 const char not_well_formed_submod_section[] = {
-    0xa1, 0x14, 0x1a, 
+    0xa1, 0x14, 0x1f, 
 };
 
